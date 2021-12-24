7c7ef54fc94ff1cac0ed7a1e9157a038c6784402
Albert Lei <albert.lei@hotmail.com>
2021-12-24 23:47:09 +0300

    add 3 Models

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.fields import CharField, DateTimeField, EmailField, TextField
 
-# Create your models here.
+class Post(models.Model):
+    def __str__(self) -> str:
+        return super().__str__()
+    title = CharField(max_length=200,null=False)
+    pub_date = DateTimeField('date published')
+    content = TextField(max_length=10000)
+
+
+class Tag(models.Model):
+    def __str__(self) -> str:
+        return super().__str__()
+    tag_name = CharField(max_length=20)
+
+class Comment(models.Model):
+    def __str__(self) -> str:
+        return super().__str__()
+    nice_name = CharField(max_length=20)
+    email = EmailField()
+    comment = CharField(max_length=400)
